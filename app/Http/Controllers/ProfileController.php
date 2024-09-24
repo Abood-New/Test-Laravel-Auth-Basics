f67057255d7df98209243136ebe928a8a33cf4cf
Abdalrhman-Jabri <abdalrhmanjabri08@gmail.com>
2024-09-24 20:39:57 +0300

    first attempt

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,6 +13,16 @@ class ProfileController extends Controller
 
     public function update(ProfileUpdateRequest $request)
     {
+        $user = auth()->user();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->input('password'));
+        }
+
+        $user->save();
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
 
